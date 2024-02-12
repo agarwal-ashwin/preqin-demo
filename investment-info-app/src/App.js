da227148244b624ca693de4e2909e09bdd0a4bcb
Ashwin Agarwal <ashwin.agarwal@hilabs.com>
2024-02-13 02:51:17 +0530

    react-router Routing

--- investment-info-app/src/App.js
+++ investment-info-app/src/App.js
@@ -1,8 +1,19 @@
-function App() {
+import './index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Details from './scenes/Investor/Details';
+import List from './scenes/Investor/List';
+
+const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+    <Box>
+      <Routes>
+        <Route path="/" exact element={<List/>} />
+        <Route path="/investors/:id" element={<Details/>} />
+      </Routes>
+    </Box>
+    </BrowserRouter>
   );
 }
 
